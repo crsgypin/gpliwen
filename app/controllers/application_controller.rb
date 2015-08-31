f34c2437d64c82f579e667bb8c5286a600bc3b91
ChiuGypin <crsgypin@gmail.com>
2015-08-31 12:46:25 +0800

    new page and navigation bar for mobile

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,10 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def mobile_device?
+		return (request.user_agent.downcase =~ /mobile|iphone|android/) ? true : false
+  end
+    
+  helper_method :mobile_device?
 end
