179a36ef45881b0cfff70e4a9451cb736f06250f
ChiuGypin <crsgypin@gmail.com>
2015-08-31 11:39:13 +0800

    table to div

--- app/helpers/deal_records_helper.rb
+++ app/helpers/deal_records_helper.rb
@@ -1,7 +1,7 @@
 module DealRecordsHelper
 
 	def day_list
-		(0..7).map {|d| [d.day.ago.strftime("%m月%d日"), d.day.ago]}
+		(0..7).map {|d| ["#{d.day.ago.strftime('%m月%d日')}", d.day.ago]}
 	end
 
 end
