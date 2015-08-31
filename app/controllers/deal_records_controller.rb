1e8c198c21723566f62b10eccbbeb2919e2a4c69
ChiuGypin <crsgypin@gmail.com>
2015-08-31 09:13:44 +0800

    complete create, show and index of deal_record

--- app/controllers/deal_records_controller.rb
+++ app/controllers/deal_records_controller.rb
@@ -4,6 +4,10 @@ class DealRecordsController < ApplicationController
 		@deal_records = DealRecord.includes(:deal_items)
 	end
 
+	def show
+		@deal_record = DealRecord.find(params[:id])
+	end
+
 	def new
 		@deal_record = DealRecord.new
 		@deal_item = @deal_record.deal_items.new
@@ -24,8 +28,8 @@ private
 	def deal_record_params
 		params.require(:deal_record).permit(
 										:date,:location,:description,
-										:deal_items_attributes=>[:id, :amount, :description, :first_category_id,:second_category_id, :third_category_id],
-										:deal_payments_attributes=>[:id, :amount, :account_entity_id, :account_owner_id])
+										:deal_items_attributes=>[:id, :amount, :description, :first_category_id,:second_category_id, :third_category_id,
+										:deal_payments_attributes=>[:id, :amount, :account_entity_id, :account_owner_id]])
 	end
 
 end
--- app/models/deal_record.rb
+++ app/models/deal_record.rb
@@ -5,9 +5,22 @@ class DealRecord < ActiveRecord::Base
 	has_many :deal_payments, :through=>:deal_items
 	accepts_nested_attributes_for :deal_payments, allow_destroy: true
 
+	before_save :check_deal_item_amount
 	before_save :calculate_amount
 
+	def total_first_category
+		self.deal_items.map{|deal_item| deal_item.first_category.name}.uniq.join(', ')
+	end
+
 private
+	def check_deal_item_amount
+		self.deal_items.each do |deal_item|
+			if deal_item.amount==nil 
+				deal_item.amount = 0
+			end
+		end
+	end
+
 	def calculate_amount
 		self.amount = self.deal_items.inject(0){|sum, deal_item| sum + deal_item.amount}
 	end
